8c95d48de9984dfcf63e39c3140463257b918f2c
katkt123 <lamkiet0902741206@gmail.com>
2024-05-02 08:34:51 +0700

    Day code

---
+++ src/main/java/com/example/QLThanhVien/QlThanhVienApplication.java
@@ -0,0 +1,13 @@
+package com.example.QLThanhVien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlThanhVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlThanhVienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QLThanhVien/QlThanhVienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QLThanhVien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlThanhVienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
