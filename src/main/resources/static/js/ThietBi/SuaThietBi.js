ba978ebcfb958ab92d3f7ea79b404bd37373337c
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2024-05-06 14:24:58 +0700

    Hiện đ

--- src/main/resources/static/js/ThietBi/SuaThietBi.js
+++ src/main/resources/static/js/ThietBi/SuaThietBi.js
@@ -12,10 +12,41 @@ function saveChanges() {
     closeForm();
 }
 
-// Lấy tất cả các phần tử có class "custom-button"
-var buttons = document.querySelectorAll(".openFormButton");
+document.addEventListener('DOMContentLoaded', function () {
+    // Lấy tất cả các nút "Sửa"
+    var editButtons = document.querySelectorAll('.openFormButton');
 
-// Lặp qua từng nút và thêm sự kiện click cho mỗi nút
-buttons.forEach(function(button) {
-    button.addEventListener("click", openForm);
+    // Lặp qua từng nút "Sửa"
+    editButtons.forEach(function (button) {
+        // Bắt sự kiện click
+        button.addEventListener('click', function () {
+            // Lấy ID của thiết bị từ thuộc tính data
+            var deviceId = button.closest('tr').getAttribute('idtb');
+            var deviceName = button.closest('tr').getAttribute('tentb');
+            var deviceDescription = button.closest('tr').getAttribute('motatb');
+
+            openForm();
+            // Lấy phần tử input bằng id
+
+            var deviceIdInput = document.getElementById('deviceCode');
+            var deviceNameInput = document.getElementById('deviceName');
+            var deviceDescriptionInput = document.getElementById('deviceDescription');
+
+            // Kiểm tra xem phần tử có tồn tại không
+            if (deviceIdInput && deviceNameInput && deviceDescriptionInput) {
+                // Set giá trị mới cho trường input
+                deviceIdInput.value = deviceId;
+                deviceNameInput.value = deviceName;
+                deviceDescriptionInput.value = deviceDescription;
+            }
+            
+        });
+    });
 });
+
+
+//
+//// Lặp qua từng nút và thêm sự kiện click cho mỗi nút
+//buttons.forEach(function(button) {
+//    button.addEventListener("click", openForm);
+//});
