6fcdee842ea8aea0339da245a52e481ec99bc939
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2024-05-07 21:12:08 +0700

    Chỉnh giao diện xóa thiết bị

--- src/main/resources/static/js/ThietBi/SuaThietBi.js
+++ src/main/resources/static/js/ThietBi/SuaThietBi.js
@@ -1,4 +1,7 @@
 
+// Danh sách các id mà người dùng đã thực hiện check vào
+const list_id_check = []
+
 // Hiện form lên 
 function openForm() {
     document.getElementById("editForm").style.display = "block";
@@ -8,6 +11,7 @@ function openForm() {
 function closeForm() {
     document.getElementById("editForm").style.display = "none";
 }
+
 
 // Thực hiện thay đổi thông tin thiết bị
 function saveChanges() {
@@ -30,8 +34,12 @@ function saveChanges() {
         })
         .then(response => {
             if (response.ok) {
+                // // Sau khi lưu xong, bạn có thể đóng form bằng cách gọi hàm closeForm()
+                closeForm();
+
                 alert("Dữ liệu đã được sửa thành công");
                 window.location.reload(); // Làm mới trang sau khi hiển thị thông báo
+                
             } else {
                 console.error('Lỗi khi lưu thiết bị');
             }
@@ -42,10 +50,52 @@ function saveChanges() {
 
         
 
-        // // Sau khi lưu xong, bạn có thể đóng form bằng cách gọi hàm closeForm()
-        closeForm();
+        
     }
 }
+
+
+function deleteThietBi() {
+
+    if (list_id_check.length == 0){
+        alert("Vui lòng chọn thiết bị muốn xóa");
+
+        return;
+    }
+
+    const confirmation = confirm("Bạn có chắc chắn muốn xóa không?");
+
+    if (confirmation) {
+        fetch('https://example.com/api/resources', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(idsToDelete) // Chuyển danh sách ID thành JSON và gửi đi
+        })
+        .then(response => {
+            if (response.ok) {
+
+                alert("Đã xóa thành công");
+                window.location.reload(); // Làm mới trang sau khi hiển thị thông báo
+                
+            } else {
+                console.error('Lỗi khi lưu thiết bị');
+            }
+            
+        })
+        .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+        });
+    } else {
+        console.log("Không xóa")
+    }
+
+    
+}
+
+
+
 
 // thực hiện đổ dữ liệu lên 
 document.addEventListener('DOMContentLoaded', function () {
@@ -82,8 +132,91 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-//
-//// Lặp qua từng nút và thêm sự kiện click cho mỗi nút
-//buttons.forEach(function(button) {
-//    button.addEventListener("click", openForm);
-//});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Lấy tất cả check box
+    var checkbox = document.querySelectorAll(".checkbox_ID");
+
+    console.log(checkbox.length)
+
+    // Lặp qua từng checkbox
+    checkbox.forEach(function (check){
+        check.addEventListener('change', function(){
+            
+            var id = check.value
+
+            if (check.checked){
+
+                // Thêm id vào danh sách check
+                list_id_check.push(id)
+
+                console.log(list_id_check)
+
+
+            }
+            else{
+                const indexToRemove = list_id_check.indexOf(id);
+
+                if (indexToRemove !== -1) {
+                    list_id_check.splice(indexToRemove, 1);
+                }
+                
+                console.log(list_id_check)
+
+            }
+
+        });
+    });
+
+
+})
+
+// thực hiện tạo thành động khi vừa load trang
+document.addEventListener('DOMContentLoaded', function() {
+
+    var checkAll = document.getElementById("checkAll");
+
+    // Lấy tất cả check box
+    var checkbox = document.querySelectorAll(".checkbox_ID");
+
+    console.log(checkbox.length)
+
+    checkAll.addEventListener('change', function() {
+       
+
+        // Xóa toàn bộ nội dung của mảng
+        list_id_check.length = 0;
+
+        if (this.checked) {
+            console.log('CheckboxAll is checked');
+            // Thực hiện các hành động khi checkbox được chọn
+
+
+            checkbox.forEach(function (check) {
+                
+                check.checked = true
+
+                var id = check.value
+                
+                list_id_check.push(id)
+                
+            });
+
+        } else {
+            console.log('CheckboxAll is unchecked');
+            // Thực hiện các hành động khi checkbox không được chọn
+
+            checkbox.forEach(function (check) {
+                    
+                check.checked = false
+
+
+            });
+
+        }
+
+        console.log(list_id_check)
+
+    });
+
+});
