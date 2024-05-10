eaed1d0870f52044ab50141f8ca4e600720aa3d5
Danh <danh123098@gmail.com>
2024-05-10 21:34:02 +0700

    update

--- src/main/java/com/example/QLThanhVien/Repository/ThanhVienRepository.java
+++ src/main/java/com/example/QLThanhVien/Repository/ThanhVienRepository.java
@@ -1,15 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.QLThanhVien.Repository;
-
-import com.example.QLThanhVien.Enity.ThanhVienEnity;
+import com.example.QLThanhVien.Enity.ThanhVienEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository; 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  *
  * @author ASUS
  */
 
-public interface ThanhVienRepository extends CrudRepository<ThanhVienEnity,Long>{}
+@Repository
+public interface ThanhVienRepository extends CrudRepository<ThanhVienEntity,Long>{
+    @Query("SELECT ttsd.id FROM ThongTinSuDungEntity ttsd WHERE ttsd.MaTV.MaTV = :id AND ttsd.TGVao IS NOT NULL")
+    List<Integer> getIDByMaTV(Integer id);
+}
