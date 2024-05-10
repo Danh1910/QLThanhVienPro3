b74bddc81a2e1cc5d2566987bc40b3d8c254ef43
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2024-05-10 19:55:03 +0700

    Xong roi Duy

--- src/main/java/com/example/QLThanhVien/Repository/ThanhVienRepository.java
+++ src/main/java/com/example/QLThanhVien/Repository/ThanhVienRepository.java
@@ -7,6 +7,7 @@ package com.example.QLThanhVien.Repository;
 import com.example.QLThanhVien.Enity.ThanhVienEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import java.util.List;
  * @author ASUS
  */
 
+@Repository
 public interface ThanhVienRepository extends CrudRepository<ThanhVienEntity,Long>{
     @Query("SELECT ttsd.id FROM ThongTinSuDungEntity ttsd WHERE ttsd.MaTV.MaTV = :id AND ttsd.TGVao IS NOT NULL")
     List<Integer> getIDByMaTV(Integer id);
